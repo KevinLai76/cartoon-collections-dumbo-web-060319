402d9d393be9027b934a6907317fcf079712c483
Kevin <kevinlai76@gmail.com>
2019-05-27 06:11:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,16 +11,16 @@ def long_planeteer_calls(array)
   array.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.each do |cheese| 
-    if array.include?(cheese)
-      return cheese
-    end
-  end
+# def find_the_cheese(array)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   cheese_types.each do |cheese| 
+#     if array.include?(cheese)
+#       return cheese
+#     end
+#   end
   
-  return nil
-end
+#   return nil
+# end
 
 def find_the_cheese(array)
   cheeses = [gouda cheddar camembert]
